34bbb8b8182a88d8210082882c8dcfb1f12fd209
falcro02 <alessio.caggiano@gmail.com>
2025-12-09 12:38:31 +0100

    Eliminate Auth and Api service in amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,31 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "cookingapi": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    }
-  },
-  "auth": {
-    "cookingwebappedcc5497": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  },
-  "function": {
-    "mealsFunction": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  },
   "storage": {
     "MealsDB": {
       "Arn": "string",
