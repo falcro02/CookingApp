53508d64974d9c08296fc4b7d22a532c8cf5e928
Fostidich <francesco.ostidich@gmail.com>
2026-03-04 22:01:47 +0100

    refactor(layout): modularised floating frame

--- frontend/src/components/FloatFoot.tsx
+++ frontend/src/components/FloatFoot.tsx
@@ -9,45 +9,58 @@ const FloatFoot = ({ appearance, setAppearance }) => (
         right: 10,
     }}>
         <Flex direction="column" gap="4">
-            <IconButton {...iconButtonProps} onClick={() =>
-                setAppearance((prev: string) => (prev === 'dark' ? 'light' : 'dark'))
-            }>
-                {appearance === 'dark' ? (
-                    <SunIcon width="20" height="20" />
-                ) : (
-                    <MoonIcon width="20" height="20" />
-                )}
-            </IconButton>
-            <Link href="https://github.com/falcro02/CookingApp" target="_blank" underline="none" >
-                <IconButton {...iconButtonProps}>
-                    <GitHubLogoIcon width="20" height="20" />
-                </IconButton>
-            </Link>
-            <Popover.Root>
-                <Popover.Trigger>
-                    <IconButton {...iconButtonProps}>
-                        <InfoCircledIcon width="20" height="20" />
-                    </IconButton>
-                </Popover.Trigger>
-                <Popover.Content>
-                    <Text size="2">
-                        Developed by
-                        Matteo Bettiati,
-                        Alessio Caggiano,
-                        Francesco Ostidich
-                    </Text>
-                </Popover.Content>
-            </Popover.Root>
+            <AppearanceToggle appearance={appearance} setAppearance={setAppearance} />
+            <GitHubLink />
+            <InfoPopover />
         </Flex>
     </Card >
-);
+)
+
+const AppearanceToggle = ({ appearance, setAppearance }) => (
+    <IconButton {...iconButtonProps} onClick={() =>
+        setAppearance((prev: string) => (prev === 'dark' ? 'light' : 'dark'))
+    }>
+        {
+            appearance === 'dark'
+                ? (<SunIcon {...iconSize} />)
+                : (<MoonIcon {...iconSize} />)
+        }
+    </IconButton>
+)
+
+const GitHubLink = () => (
+    <Link href="https://github.com/falcro02/CookingApp" target="_blank" underline="none" >
+        <IconButton {...iconButtonProps}>
+            <GitHubLogoIcon {...iconSize} />
+        </IconButton>
+    </Link>
+)
+
+const InfoPopover = () => (
+    <Popover.Root>
+        <Popover.Trigger>
+            <IconButton {...iconButtonProps}>
+                <InfoCircledIcon {...iconSize} />
+            </IconButton>
+        </Popover.Trigger>
+        <Popover.Content>
+            <Text size="2">
+                Developed by Matteo Bettiati, Alessio Caggiano, Francesco Ostidich
+            </Text>
+        </Popover.Content>
+    </Popover.Root>
+)
+
+const iconSize = {
+    width: "20",
+    height: "20",
+}
 
 const iconButtonProps = {
     variant: "ghost",
     radius: "full",
-    color: "gray",
     style: { color: "var(--gray-12)" },
-} as const;
+}
 
-export default FloatFoot;
+export default FloatFoot
 
--- frontend/src/components/FloatHead.tsx
+++ frontend/src/components/FloatHead.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, Flex, Heading, Link, Separator } from '@radix-ui/themes';
+import { Box, Button, Card, Flex, Heading, Link, Separator } from '@radix-ui/themes';
 
 const FloatHead = () => (
     <Card m="4" style={{
@@ -8,18 +8,10 @@ const FloatHead = () => (
         left: 10,
     }}>
         <Box width="20vw">
-            <Flex direction="row" align="center" gap="4" mb="2">
-                <HomeLink>
-                    <Box p="1">
-                        <img src="/icon.webp" alt="Logo" width="40" />
-                    </Box>
-                </HomeLink>
-                <HomeLink>
-                    <Heading size="6">Cooking</Heading>
-                </HomeLink>
-            </Flex>
-            <Separator size="4" />
-            <Flex direction="column" gap="2" p="4">
+            <LogoTitle />
+            <Separator size="4" my="2" />
+            <Flex direction="column" gap="3" p="4">
+                {/* TODO: keep selected colored */}
                 <PageLink name="Groceries list" ref="#shopping" />
                 <PageLink name="Meal ideas" ref="#ideas" />
                 <PageLink name="Profile" ref="#profile" />
@@ -27,21 +19,28 @@ const FloatHead = () => (
             <Box height="3em" />
         </Box>
     </Card>
-);
+)
+
+const LogoTitle = () => (
+    <Flex direction="row" align="center" gap="4">
+        <Link href="#home" underline="none">
+            <Box p="1">
+                <img src="/icon.webp" alt="Logo" width="40" />
+            </Box>
+        </Link>
+        <Heading size="6">Cooking</Heading>
+    </Flex>
+)
 
 const PageLink = ({ name, ref }) => (
-    <Link href={ref} size="3" weight="medium" underline="always" style={{
-        color: "var(--gray-12)",
-        textDecorationColor: "var(--gray-8)",
-    }}>{name}</Link>
+    <Link href={ref} underline="none" >
+        <Button variant="ghost" radius="full" style={{ color: "var(--gray-12)" }}>
+            <Heading size="3" weight="medium">
+                {name}
+            </Heading>
+        </Button>
+    </Link >
 )
 
-const HomeLink = ({ children }) => (
-    <Link href="#home" style={{
-        color: "var(--gray-12)",
-        textDecorationColor: "var(--gray-8)",
-    }}>{children}</Link>
-)
-
-export default FloatHead;
+export default FloatHead
 
